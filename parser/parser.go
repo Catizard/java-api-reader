03c50758272c90f8e7d0c4dacb858a6b3b7edcde
Catizard <1185032459@qq.com>
2023-02-27 22:06:58 +0800

    完成简单的对Controller代码的解析

--- parser/parser.go
+++ parser/parser.go
@@ -10,19 +10,22 @@ import (
 )
 
 type Param struct {
-	pType string
+	pAnno   string
+	pType   string
+	comment string
 }
 
 type ApiInfo struct {
+	retType string
 	apiName string
 	params  []Param
-	retType string
 	usage   string
 }
 
 type ControllerApiInfo struct {
 	ApiInfo
-	Address string
+	Address    string
+	MethodType string
 }
 
 type Parser struct {
@@ -58,6 +61,7 @@ func (p *Parser) Parse(file *os.File) error {
 			if len(line) > 0 && line[0] == '@' {
 				if isControllerClass(line) {
 					p.doParseController(reader)
+					log.Printf("[DEBUG] after doParseController, p.controllerApiInfos=%v\n", p.controllerApiInfos)
 				}
 			}
 		}
@@ -70,7 +74,64 @@ func (p *Parser) Parse(file *os.File) error {
 }
 
 func extractMethod(signature string) (ApiInfo, error) {
-	return ApiInfo{}, fmt.Errorf("TODO")
+	apiInfo := ApiInfo{}
+	apiInfo.Init()
+	posL := strings.Index(signature, "(")
+	posR := strings.LastIndex(signature, ")")
+	if posL == -1 || posR == -1 {
+		return apiInfo, fmt.Errorf("extractMethod failed with signature = %v, cannot find any of '(' or ')'\n", signature)
+	}
+	info := signature[:posL]
+	params := signature[posL+1 : posR]
+	doExtractMethodInfo(info, &apiInfo)
+	doExtractMethodParams(params, &apiInfo)
+	return apiInfo, nil
+}
+
+func doExtractMethodInfo(info string, apiInfo *ApiInfo) {
+	ss := strings.Split(info, " ")
+	// [access type] [return type] [method name]
+	if len(ss) < 2 {
+		log.Fatalf("I think something is wrong with doExtractMethodInfo?")
+		return
+	}
+
+	// we dont need the public or private info
+	if len(ss) > 2 {
+		ss = ss[1:]
+	}
+
+	apiInfo.retType = ss[0]
+	apiInfo.apiName = ss[1]
+}
+
+func doExtractMethodParams(params string, apiInfo *ApiInfo) {
+	if params == "" {
+		return
+	}
+
+	ss := strings.Split(params, ",")
+	if len(ss) == 0 {
+		return
+	}
+
+	for _, v := range ss {
+		// every v of ss is a param
+		param := Param{}
+		sv := strings.Split(v, " ")
+		if len(sv) < 2 || len(sv) > 3 {
+			log.Fatalf("I think something is wrong with doExtractMethodParams")
+			return
+		}
+
+		if len(sv) == 3 {
+			param.pAnno = sv[0]
+			sv = sv[1:]
+		}
+
+		param.pType = sv[0]
+		apiInfo.params = append(apiInfo.params, param)
+	}
 }
 
 func isControllerClass(line string) bool {
@@ -86,6 +147,8 @@ func (p *Parser) doParseController(reader *bufio.Reader) {
 
 	afterClass := false
 	baseUrl := ""
+	methodUrl := ""
+	methodType := "any"
 	readyForMethod := false
 	for {
 		// TODO 如果一行内容被拆分？如何正确的组装
@@ -102,22 +165,24 @@ func (p *Parser) doParseController(reader *bufio.Reader) {
 		line = strings.Trim(line, " ")
 		line = strings.Replace(line, "\r\n", "", -1)
 
+		// TODO 如果发现javadoc，或者其次只是普通的注释，应该怎样记录下来？
 		if readyForMethod {
 			if !strings.HasPrefix(line, "public") {
 				continue
 			}
 			// TODO extract method info
-			log.Printf("caught method line is {%v}\n", line)
+			//log.Printf("caught method line is {%v}\n", line)
 			apiInfo, err := extractMethod(line)
 			if err != nil {
 				fmt.Printf("extract method failed with %v\n", err)
 				return
 			}
-			controllerApiInfo := &ControllerApiInfo{apiInfo, "[TODO]"}
+			controllerApiInfo := &ControllerApiInfo{apiInfo, baseUrl + methodUrl, methodType}
 			if _, ok := p.controllerApiInfos[baseUrl]; !ok {
 				p.controllerApiInfos[baseUrl] = make([]ControllerApiInfo, 0)
 			}
 			p.controllerApiInfos[baseUrl] = append(p.controllerApiInfos[baseUrl], *controllerApiInfo)
+			//log.Printf("caught a ControllerApiInfo with baseUrl=%v, info=%v\n", baseUrl, controllerApiInfo)
 			readyForMethod = false
 			continue
 		}
@@ -126,11 +191,10 @@ func (p *Parser) doParseController(reader *bufio.Reader) {
 			// catch class info
 			if strings.HasPrefix(line, "@RequestMapping") {
 				baseUrl = strings.Split(line, "\"")[1]
-				log.Printf("[DEBUG] catch baseUrl = {%v}\n", baseUrl)
 			}
 
 			if strings.HasPrefix(line, "public class") {
-				log.Printf("read class line is {%v}\n", line)
+				//log.Printf("read class line is {%v}\n", line)
 				afterClass = true
 			}
 		} else {
@@ -140,9 +204,22 @@ func (p *Parser) doParseController(reader *bufio.Reader) {
 			if strings.HasPrefix(line, "@RequestMapping") ||
 				strings.HasPrefix(line, "@GetMapping") ||
 				strings.HasPrefix(line, "@PostMapping") {
-				log.Printf("[DEBUG] caught a mapping annotaion, line is {%v}\n", line)
+				//method type
+				pos := strings.Index(line, "Mapping")
+				methodType = line[1:pos]
+				//method url
+				ss := strings.Split(line, "\"")
+				if len(ss) != 3 {
+					log.Fatalf("split method url failed, len != 3")
+				}
+				methodUrl = ss[1]
+
 				readyForMethod = true
 			}
 		}
 	}
+}
+
+func (p Param) String() string {
+	return "pAnno = " + p.pAnno + ", pType = " + p.pType
 }
